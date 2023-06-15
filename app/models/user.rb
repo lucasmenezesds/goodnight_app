51a9affac675b9f3807f7306ed734592dc7aed7c
Lucas Menezes <menezes.lucasdesouza@gmail.com>
2023-06-15 18:25:07 +0900

    Improved db relationship names, created  follow unfollow methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,27 @@ class User < ApplicationRecord
 
   has_many :sleep_logs, dependent: :destroy
 
-  has_many :is_following, class_name: 'Relationship', foreign_key: 'source_id', inverse_of: :source, dependent: :destroy
-  has_many :is_followed, class_name: 'Relationship', foreign_key: 'target_id', inverse_of: :target, dependent: :destroy
+  has_many :following_relationships, class_name: 'Relationship', foreign_key: 'source_id', inverse_of: :source, dependent: :destroy
+  has_many :followed_relationships, class_name: 'Relationship', foreign_key: 'target_id', inverse_of: :target, dependent: :destroy
 
-  has_many :following, through: :is_following, source: :target
-  has_many :followers, through: :is_followed, source: :source
+  has_many :following, through: :following_relationships, source: :target
+  has_many :followers, through: :followed_relationships, source: :source
+
+  def follow(user_to_follow)
+    following = following_relationships.with_deleted.find_by(target_id: user_to_follow.id)
+    if following.present? && following.deleted?
+      following.restore
+    else
+      following_relationships.create(target_id: user_to_follow.id)
+    end
+  end
+
+  def unfollow(user_to_unfollow)
+    # noinspection RubyRedundantSafeNavigation
+    following_relationships.find_by(target_id: user_to_unfollow.id)&.destroy
+  end
+
+  def following?(user)
+    following.include?(user)
+  end
 end
