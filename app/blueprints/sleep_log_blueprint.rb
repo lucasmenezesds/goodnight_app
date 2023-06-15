9d669e65e218f6e20f2140695a0aa29968b741c3
Lucas Menezes <menezes.lucasdesouza@gmail.com>
2023-06-15 23:10:25 +0900

    Improved sleeplog endpoint response by adding user name
    - added user name to the endpoint to makes easier to identify the user

--- app/blueprints/sleep_log_blueprint.rb
+++ app/blueprints/sleep_log_blueprint.rb
@@ -3,6 +3,12 @@
 class SleepLogBlueprint < Blueprinter::Base
   identifier :uuid
 
+  view :with_user_name do
+    field :user_name do |log, _options|
+      log.user.name
+    end
+  end
+
   field :user_id
   field :slept_at, datetime_format: TimeConcern::STANDARD_DATETIME_FORMAT
   field :woke_up_at, datetime_format: TimeConcern::STANDARD_DATETIME_FORMAT
